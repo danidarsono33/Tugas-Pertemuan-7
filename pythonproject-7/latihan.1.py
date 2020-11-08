57708bf054f31a4116da5b1a9ae76ab5f0e5e100
danidarsono33 <danidarsono33@gmail.com>
2020-11-09 01:48:12 +0700

    first commit

---
+++ pythonproject-7/latihan.1.py
@@ -0,0 +1,9 @@
+a = 10
+b = a
+for x in range (a):
+    for y in range(b):
+        z = x+y
+        print("{0:5}".format (z),end='')
+
+    print()
+
---
+++ pythonproject-7/latihan.3_2.py
@@ -0,0 +1,8 @@
+x=0
+while True:
+    a=int(input("Masukan bilangan :"))
+    if x<a:
+        x=a
+    if a==0:
+        break
+print ("bilangan terbesar adalah =",x)
